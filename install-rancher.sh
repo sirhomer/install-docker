609da7728522c6f2d3e7ef232abc43f492f02de8
sirhomer <cjmbaires@gmail.com>
2023-05-11 22:34:41 -0300

    Update and rename install.sh to install-rancher.sh

--- install-rancher.sh
+++ install-rancher.sh
@@ -15,6 +15,4 @@ apt install sudo -y
 
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker run --privileged -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
