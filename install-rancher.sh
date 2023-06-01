b0c036c41a8becb1324445e96f472b88d230ad6a
sirhomer <cjmbaires@gmail.com>
2023-06-01 00:36:22 -0300

    Actualizar install-rancher.sh

--- install-rancher.sh
+++ install-rancher.sh
@@ -16,3 +16,6 @@ apt install sudo -y
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 #sudo docker run --privileged -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
+
+
+
