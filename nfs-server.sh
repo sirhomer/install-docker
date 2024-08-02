b3d4d4c5ecca480c8209cd61e1a8945c3d5a3265
sirhomer <cjmbaires@gmail.com>
2024-08-02 12:56:11 -0300

    Update nfs-server.sh
    permisos

--- nfs-server.sh
+++ nfs-server.sh
@@ -1,4 +1,4 @@
 mkdir /sitios
 apt -y install nfs-kernel-server
-echo ' /sitios   192.168.100.0/24(rw,sync,no_subtree_check)' >>/etc/exports
+echo ' /sitios 192.168.1.0/24(rw,sync,no_subtree_check,no_root_squash,anonuid=1000,anongid=1000)' >>/etc/exports
 systemctl restart nfs-server
